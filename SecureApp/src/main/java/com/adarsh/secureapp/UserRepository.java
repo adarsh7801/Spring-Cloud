5bc5d43d2bd2ff86117d5cb74dd465c787c8012b
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-03-20 11:21:56 +0530

    Adarsh add . springboot security using OAUTH2

---
+++ SecureApp/src/main/java/com/adarsh/secureapp/UserRepository.java
@@ -0,0 +1,8 @@
+package com.adarsh.secureapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
+
+}
